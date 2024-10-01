7e18958b031ba09e3fa3d62d678b815b7ae1c50a
Hanna Juvonen <hanna.juvonen@myy.haaga-helia.fi>
2024-10-01 12:15:49 +0300

    Tehtavan palautus

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+import * as Speech from "expo-speech";
+import { useState } from "react";
 
 export default function App() {
+  const [text, setText] = useState("");
+
+  const speak = () => {
+    Speech.speak(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter some text"
+        onChangeText={(text) => setText(text)}
+        value={text}
+      />
+      <Button title="PRESS TO HEAR TEXT" onPress={speak} />
     </View>
   );
 }
@@ -13,8 +26,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 80,
+    width: 280,
+    borderWidth: 1,
+    padding: 10,
+    fontSize: 18,
   },
 });
